effb83ccea9f1bda1a107faded53cfdc74cbd082
RajendraKSahoo <45260260+RajendraKSahoo@users.noreply.github.com>
2018-12-28 22:52:25 +0530

    Add files via upload

--- Program1.java
+++ Program1.java
@@ -1,32 +1,53 @@
 package demo1;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Program1 {
-	public static void main (String arg[])
-	{
+	public static void main(String arg[]) throws InterruptedException {
+		
 		System.out.println("Start of coding...");
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("http://datatables.net");
+		WebDriverWait wait = new WebDriverWait(driver, 60);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+				
+		driver.get("https://datatables.net/");
 		
-		WebElement showEle = driver.findElement(By.xpath("//select[@name='example_length']/option[3]"));
+		WebElement showEle= driver.findElement(By.xpath("//select[@name='example_length']/option[3]"));
 		showEle.click();
+		System.out.println("The count for show entries is selected as 50...");
 		
-		//RowCount
-		WebElement rows = driver.findElement(By.xpath("//table[@id='example']/tbody/tr"));
-		//int rowCount = rows.getSize();
+		List<WebElement> rows= driver.findElements(By.xpath("//table[@id='example']/tbody/tr"));
+		int noOfRows=rows.size();
 		
+		int total=0;
+		for(int i=1;i<=noOfRows;i++)
+		{
+		WebElement nameEle= driver.findElement(By.xpath("//table[@id='example']/tbody/tr["+i+"]/td[1]"));
+		nameEle.click();
 		
-		//WebElement nameEle = driver.findElements(By.xpath(""));
+		WebElement ageEle= driver.findElement(By.xpath("//table[@id='example']/tbody/tr["+i+"]/td[4]"));
+		String age = ageEle.getText();
+		int Age = Integer.parseInt(age);
+		System.out.println(Age);
 		
+		WebElement salEle = driver.findElement(By.xpath("//span[@class='dtr-data']"));
+		String salary = salEle.getText().trim().replace("$", "").replace(",", "");
+		int Salary = Integer.parseInt(salary);
+		System.out.println(Salary);
+		nameEle.click();
+		}
 		
+		System.out.println("The total salary of all the employee: " +total);
+		
+		System.out.println("End of Program!!!");
 	}
-
 }
--- Program2.java
+++ Program2.java
@@ -1,21 +1,53 @@
 package demo1;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Program2 {
-	public static void main (String arg[])
-	{
+	public static void main (String arg[]) throws InterruptedException {
 		System.out.println("Start of coding...");
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.moneycontrol.com/");
-		
 				
+		WebDriver driver = new ChromeDriver();
+		WebDriverWait wait = new WebDriverWait(driver, 60);
+		
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+				
+		driver.get("https://moneycontrol.com/");
+		Thread.sleep(1000);
+		
+		WebElement stocksEle = driver.findElement(By.xpath("(//a[@title='Stocks'])[1]"));
+		stocksEle.click();
+		Thread.sleep(1000);
+		WebElement alphaEle = driver.findElement(By.xpath("//a[@title='A']"));
+		alphaEle.click();
+		
+		List<WebElement> alinksEle=driver.findElements(By.xpath("//table[@class='pcq_tbl MT10']/tbody/tr/td"));
+		int noOfLinks = alinksEle.size();
+		System.out.println("The total number of links starting with A: " +noOfLinks);
+		System.out.println("The links are as below:");
+		
+		for(int i=0; i<noOfLinks; i++)
+		{
+			WebElement linkEle= alinksEle.get(i);
+			String link = linkEle.getAttribute("href");
+						
+			String text= linkEle.getText();
+			System.out.println(text);
+			
+		}
+		
+		WebElement alEle = driver.findElement(By.xpath("//a[@title='Ashok Leyland']"));
+		alEle.click();
+		String title = driver.getTitle();
+		System.out.println("Title of the page: "+title);
+		
 	}
-
 }
